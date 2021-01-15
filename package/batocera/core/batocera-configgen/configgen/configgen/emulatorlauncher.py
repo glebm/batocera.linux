78a7632e0c1bb2f463e60ee839dce0ab38114d2e
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-15 16:29:26 +0000

    openmw: Add EmulationStation skeleton
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/emulatorlauncher.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/emulatorlauncher.py
@@ -47,6 +47,7 @@ from generators.mugen.mugenGenerator import MugenGenerator
 from generators.lightspark.lightsparkGenerator import LightsparkGenerator
 from generators.ruffle.ruffleGenerator import RuffleGenerator
 from generators.duckstation.duckstationGenerator import DuckstationGenerator
+from generators.openmw.openmwGenerator import OpenMWGenerator
 
 import controllersConfig as controllers
 import signal
@@ -97,6 +98,7 @@ generators = {
     'lightspark': LightsparkGenerator(),
     'ruffle': RuffleGenerator(),
     'duckstation': DuckstationGenerator(),
+    'openmw': OpenMWGenerator(),
 }
 
 def main(args, maxnbplayers):
