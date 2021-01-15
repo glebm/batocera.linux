0b07de646d851613eff1fdeaeaaf88e7773a3ce8
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-15 16:29:26 +0000

    openmw: Add EmulationStation skeleton
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/openmw/openmwGenerator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import re
+import shutil
+
+import Command
+from generators.Generator import Generator
+import controllersConfig
+
+class OpenMWGenerator(Generator):
+
+    def generate(self, system, rom, playersControllers, gameResolution):
+      pass
