9e387593b9a1d711fc8db95e25045c2abc6d2948
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-16 14:44:29 +0000

    openmw: Use the new static deps features
    1. Massively simplifies the config
    2. LTO now works (though the unity build now doesn't)

--- package/batocera/emulators/openmw/openmw/openmw.mk
+++ package/batocera/emulators/openmw/openmw/openmw.mk
@@ -4,17 +4,57 @@
 #
 ################################################################################
 
-OPENMW_VERSION = 7455dfb3a18ef57128f3f96d1d41bd5899e33aa0
+# NOTE: When bumping the version of this package, make sure that
+# `OPENMW_EXTRA_DOWNLOADS` are in sync with openmw's `extern/CMakeLists.txt`.
+
+# https://gitlab.com/OpenMW/openmw/-/merge_requests/547
+OPENMW_VERSION = 8cb654509979469ea89f08908b67bc33eab11078
 OPENMW_SOURCE = openmw-$(OPENMW_VERSION).tar.gz
-OPENMW_SITE = https://gitlab.com/OpenMW/openmw/-/archive/$(OPENMW_VERSION)
-
+OPENMW_SITE = https://gitlab.com/glebm/openmw/-/archive/$(OPENMW_VERSION)
 OPENMW_LICENSE = GPL-3.0+
-OPENMW_DEPENDENCIES = sdl2 bullet freetype fontconfig libpng jpeg boost ffmpeg lz4 osg-openmw mygui-openmw openal
+OPENMW_DEPENDENCIES = sdl2 freetype fontconfig libpng jpeg boost ffmpeg lz4 openal
 
-OPENMW_CONF_OPTS = \
+# OpenMW's static build uses `FetchInclude`, which by default will download
+# dependencies from remote locations.
+#
+# We want to keep all downloads in the Buildroot's DL directory, so we download
+# them via buildroot and pass them explictly.
+OPENMW_BULLET_VERSION = ed5256454f4f84bd2c1728c88ddb0405d614e7d2
+OPENMW_MYGUI_VERSION = MyGUI3.4.0
+OPENMW_OSG_VERSION = b0259766eaee3cfc6bb523095a7e0bd395c2a4c7
+OPENMW_BULLET_SOURCE = $(OPENMW_BULLET_VERSION).tar.gz
+OPENMW_MYGUI_SOURCE = $(OPENMW_MYGUI_VERSION).tar.gz
+OPENMW_OSG_SOURCE = $(OPENMW_OSG_VERSION).tar.gz
+OPENMW_EXTRA_DOWNLOADS = \
+	$(call github,glebm,bullet3,$(OPENMW_BULLET_SOURCE)) \
+	$(call github,MyGUI,mygui,$(OPENMW_MYGUI_SOURCE)) \
+	$(call github,glebm,OpenSceneGraph,$(OPENMW_OSG_SOURCE))
+
+define OPENMW_EXTRACT_EXTRA_DOWNLOADS
+	mkdir -p $(@D)/br-fetched/bullet-$(OPENMW_BULLET_VERSION)
+	$(call suitable-extractor,$(OPENMW_BULLET_SOURCE)) $(OPENMW_DL_DIR)/$(OPENMW_BULLET_SOURCE) | \
+		$(TAR) --strip-components=1 -C $(@D)/br-fetched/bullet-$(OPENMW_BULLET_VERSION) $(TAR_OPTIONS) -
+	mkdir -p $(@D)/br-fetched/mygui-$(OPENMW_MYGUI_VERSION)
+	$(call suitable-extractor,$(OPENMW_MYGUI_SOURCE)) $(OPENMW_DL_DIR)/$(OPENMW_MYGUI_SOURCE) | \
+		$(TAR) --strip-components=1 -C $(@D)/br-fetched/mygui-$(OPENMW_MYGUI_VERSION) $(TAR_OPTIONS) -
+	mkdir -p $(@D)/br-fetched/osg-$(OPENMW_OSG_VERSION)
+	$(call suitable-extractor,$(OPENMW_OSG_SOURCE)) $(OPENMW_DL_DIR)/$(OPENMW_OSG_SOURCE) | \
+		$(TAR) --strip-components=1 -C $(@D)/br-fetched/osg-$(OPENMW_OSG_VERSION) $(TAR_OPTIONS) -
+endef
+OPENMW_POST_EXTRACT_HOOKS += OPENMW_EXTRACT_EXTRA_DOWNLOADS
+
+OPENMW_CONF_OPTS += \
+	-DFETCHCONTENT_FULLY_DISCONNECTED=ON \
+	-DFETCHCONTENT_SOURCE_DIR_BULLET=$(OPENMW_BUILDDIR)br-fetched/bullet-$(OPENMW_BULLET_VERSION) \
+	-DFETCHCONTENT_SOURCE_DIR_MYGUI=$(OPENMW_BUILDDIR)br-fetched/mygui-$(OPENMW_MYGUI_VERSION) \
+	-DFETCHCONTENT_SOURCE_DIR_OSG=$(OPENMW_BUILDDIR)br-fetched/osg-$(OPENMW_OSG_VERSION) \
+	-DOPENMW_USE_SYSTEM_BULLET=OFF \
+	-DOPENMW_USE_SYSTEM_MYGUI=OFF \
+	-DOPENMW_USE_SYSTEM_OSG=OFF
+
+OPENMW_CONF_OPTS += \
 	-DBUILD_SHARED_LIBS=OFF \
 	-OPENMW_VERSION_COMMITHASH=$(OPENMW_VERSION) \
-	-DOPENMW_UNITY_BUILD=ON \
 	-DBUILD_LAUNCHER=OFF \
 	-DBUILD_WIZARD=OFF \
 	-DBUILD_MWINIIMPORTER=OFF \
@@ -22,31 +62,49 @@ OPENMW_CONF_OPTS = \
 	-DBUILD_ESSIMPORTER=OFF \
 	-DBUILD_BSATOOL=OFF \
 	-DBUILD_ESMTOOL=OFF \
-	-DBUILD_NIFTEST=OFF \
-	-DMYGUI_STATIC=ON \
-	-DOSG_STATIC=ON \
-	-DBULLET_USE_DOUBLES=OFF
+	-DBUILD_NIFTEST=OFF
+
+# Enable link-time optimization.
+# Known to significantly improve OpenMW's performance.
+OPENMW_CONF_OPTS += \
+	-DOPENMW_LTO_BUILD=ON \
+	-DCMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE=ON \
+	-DCMAKE_POLICY_DEFAULT_CMP0069=NEW
+
+# MyGUI config options
+OPENMW_CONF_OPTS += \
+	-DMYGUI_RENDERSYSTEM=1
+
+# OpenSceneGraph config options
+OPENMW_CONF_OPTS += \
+	-DOSG_CPP_EXCEPTIONS_AVAILABLE=ON \
+	-D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS=1
+ifeq ($(BR2_PACKAGE_XORG7),y)
+OPENMW_DEPENDENCIES += xlib_libX11 xlib_libXrandr fltk
+OPENMW_CONF_OPTS += -DOSG_WINDOWING_SYSTEM=X11
+else
+OPENMW_CONF_OPTS += -DOSG_WINDOWING_SYSTEM=None
+endif
 
 # Disable common warning types to avoid polluting the build log.
 OPENMW_CONF_OPTS += -DCMAKE_CXX_FLAGS="-Wno-psabi"
 
-# Do not enable LTO (-DOPENMW_LTO_BUILD=ON) because it causes an internal compiler error with GCC 9.3.0 and 10.2.0:
-# lto1: internal compiler error: in add_symbol_to_partition_1, at lto/lto-partition.c:153
-# TODO: Try again after upgrading GCC.
-
-OPENMW_CONF_ENV += PKG_CONFIG_PATH="$(STAGING_DIR)/build/openmw/usr/lib/pkgconfig"
-
-# OpenMW does not use pkg-config to find OSG, so we need to point to it manually because it is in an unusual location.
-OPENMW_CONF_OPTS += -DOSG_DIR=$(STAGING_DIR)/build/openmw/usr
-
 ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
 OPENMW_DEPENDENCIES += libgl
+
+# OpenSceneGraph:
+OPENMW_DEPENDENCIES += libfreeglut libgtk2 # libgl already added
+OPENMW_CONF_OPTS += -DOPENGL_PROFILE=GL2
 else
+# Needed by OpenMW itself and OpenSceneGraph:
 OPENMW_DEPENDENCIES += gl4es-openmw
 OPENMW_CONF_OPTS += \
 	-DOPENGL_gl_LIBRARY=$(TARGET_DIR)/usr/lib/openmw/gl4es/libGL.so.1 \
 	-DOPENGL_glx_LIBRARY=$(TARGET_DIR)/usr/lib/openmw/gl4es/libGL.so.1 \
 	-DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include/openmw/gl4es
+
+# OpenSceneGraph:
+OPENMW_CONF_OPTS += -DOPENGL_PROFILE=GL2
 endif
 
 $(eval $(cmake-package))
